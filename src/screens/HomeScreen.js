596774294e27bcbac4981783dfef061b70e1893a
Andrew Young <andrewy445@gmail.com>
2025-01-18 22:36:50 -0500

    home stuff

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import commonStyles from "../styles/commonStyles";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -8,36 +8,40 @@ import DebtAccountCard from "../components/DebtAccountCard";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={commonStyles.container}>
-      <Text style={commonStyles.title}>Summit</Text>
+    <View style={[commonStyles.container, { flex: 1 }]}>
+      <ScrollView 
+        contentContainerStyle={{ flexGrow: 1, paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Text style={commonStyles.title}>Summit</Text>
 
-      {/* Summit Card */}
-      <SummitChart />
+        {/* Summit Card */}
+        <SummitChart />
 
-      {/* Small Cards Container */}
-      <View style={commonStyles.smallCardsContainer}>
-        {/* Payment Total Card */}
-        <View style={commonStyles.smallCard}>
-          <Text style={commonStyles.label}>Payment Total</Text>
-          <Text style={commonStyles.amount}>725</Text>
+        {/* Small Cards Container */}
+        <View style={commonStyles.smallCardsContainer}>
+          <View style={commonStyles.smallCard}>
+            <Text style={commonStyles.label}>Payment Total</Text>
+            <Text style={commonStyles.amount}>725</Text>
+          </View>
+          <View style={commonStyles.smallCard}>
+            <Text style={commonStyles.label}>Total</Text>
+            <Text style={commonStyles.amount}>10k</Text>
+          </View>
         </View>
-        {/* Total Card */}
-        <View style={commonStyles.smallCard}>
-          <Text style={commonStyles.label}>Total</Text>
-          <Text style={commonStyles.amount}>10k</Text>
+
+        <Text style={commonStyles.subTitle}>Accounts</Text>
+
+        {/* Additional Info */}
+        <View style={commonStyles.infoContainer}>
+          <DebtAccountCard type="Credit Card" amount="500" dueDate="2023-11-01" />
+          <DebtAccountCard type="Student Loan" amount="1500" dueDate="2023-12-15" />
+          <DebtAccountCard type="Auto Loan" amount="8000" dueDate="2024-01-10" />
+          <DebtAccountCard type="Mortgage" amount="200000" dueDate="2024-02-01" />
+          <DebtAccountCard type="Personal Loan" amount="3000" dueDate="2024-03-05" />
         </View>
-      </View>
+      </ScrollView>
 
-      {/* Accounts Header */}
-      <Text style={commonStyles.subTitle}>Accounts</Text>
-
-      {/* Additional Info */}
-      <View style={commonStyles.infoContainer}>
-        <DebtAccountCard type="Credit Card" amount="500" dueDate="2023-11-01" />
-        <DebtAccountCard type="Student Loan" amount="1500" dueDate="2023-12-15" />
-      </View>
-
-      {/* Plus Button */}
       <TouchableOpacity style={commonStyles.plusButton}>
         <FontAwesomeIcon icon={faPlus} size={24} color="black" />
       </TouchableOpacity>
